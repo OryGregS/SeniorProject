a05701f69c000bd62c91011b879f2d88b64ea304
axeljeremy7 <axel.jeremy.7@gmail.com>
2018-12-12 18:15:48 -0500

    refactor TestRecordMatcher - rename

--- project/src/test/java/wmu/datamatching/TestRecordMatcher.java
+++ project/src/test/java/wmu/datamatching/TestRecordMatcher.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-public class RecordMatcherTest {
+public class TestRecordMatcher {
 
 
     @Test
