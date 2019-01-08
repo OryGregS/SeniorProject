fb249c282f348c804c546a2c51b8358a33e9ccf6
Greg Smith <gskill95@gmail.com>
2019-01-08 05:51:57 -0500

    Added Version Control Standard & Copyright
    Each file containing source code now generates a header containing a standard for updating the project as well as copyright information.

--- project/src/test/java/wmu/datamatching/TestRecordMatcher.java
+++ project/src/test/java/wmu/datamatching/TestRecordMatcher.java
@@ -1,3 +1,20 @@
+/*
+ * ~~~ Version Control Standard ~~~
+ *
+ * Create a separate branch for implementing new features.
+ *
+ * *** BEFORE COMMITTING ****
+ *
+ * ---> Run all unit tests to ensure functionality
+ * ---> Run maven's clean command (mvn clean or use the tool in IDE)  to remove unnecessary files
+ * ---> Merge the branch into master once the feature is complete with unit tests.
+ *
+ * ~~~ Copyright ~~~
+ *
+ * Developed by Gregory Smith & Axel Solano. Last modified 08/01/19 5:34 AM.
+ * Copyright (c) 2019. All rights reserved.
+ */
+
 package wmu.datamatching;
 
 import java.util.ArrayList;
